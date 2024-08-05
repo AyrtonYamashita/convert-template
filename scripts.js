e5a14925795baf16e4e0f0778e479c9f55d8487e
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-08-05 10:13:13 -0400

    Restringindo caracteres no input

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const amount = document.querySelector("#amount");
+const regex_hasCharacters = /\D+/g
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-  console.log(amount.value)
+  amount.value = amount.value.replace(regex_hasCharacters, "")
 })
