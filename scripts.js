acf85f40e3be464cff91fce6654e7da4130ba73a
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-08-05 10:19:16 -0400

    Capturando o tipo de moeda

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-const amount = document.querySelector("#amount");
+const form = document.querySelector("form")
+const amount = document.querySelector("#amount")
+const currency = document.querySelector("#currency")
+
 const regex_hasCharacters = /\D+/g
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
   amount.value = amount.value.replace(regex_hasCharacters, "")
 })
+
+
+// Capturando evento de submit no formulário
+form.onsubmit = (e) => {
+  e.preventDefault()
+
+  console.log(currency.value)
+}
